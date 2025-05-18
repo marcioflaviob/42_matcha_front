1a0fa2514bf8add80343e756fce1d769b05e6a43
Marcio Flavio <oi@marcioflavio.com>
2025-05-18 18:47:17 +0200

    Add https as optional

--- vite.config.js
+++ vite.config.js
@@ -1,28 +1,37 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import { nodePolyfills } from 'vite-plugin-node-polyfills';
 import fs from 'fs';
 
-export default defineConfig({
+export default defineConfig(({ mode }) => {
+
+  const env = loadEnv(mode, process.cwd());
+  
+  const useHttps = env.VITE_USE_HTTPS === 'true';
+
+  return {
     plugins: [
-        react(),
-        nodePolyfills({
-            globals: {
-                Buffer: true,
-                global: true,
-                process: true,
-            },
-        }),
+      react(),
+      nodePolyfills({
+        globals: {
+          Buffer: true,
+          global: true,
+          process: true,
+        },
+      }),
     ],
     resolve: {
-        alias: {
-            stream: 'stream-browserify',
-        },
+      alias: {
+        stream: 'stream-browserify',
+      },
     },
     server: {
+      ...(useHttps ? {
         https: {
-            key: fs.readFileSync('./cert/key.pem'),
-            cert: fs.readFileSync('./cert/cert.pem'),
-        },
+          key: fs.readFileSync('./cert/key.pem'),
+          cert: fs.readFileSync('./cert/cert.pem'),
+        }
+      } : {})
     },
+  };
 });
